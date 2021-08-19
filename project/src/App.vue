441f0fe2e2c0b6b7331f3eb3286cf1a420f551ba
Yuri Gonçalves <65895389+yuriv15@users.noreply.github.com>
2021-08-19 02:36:46 -0300

    fixed a typo

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Check out the top 30 stories!"/>
+    <HelloWorld msg="Check out the top 20 stories!"/>
   </div>
 </template>
 
